f042edd582bca6b20f7e8d1d13af9d11d8f8dd08
Sreeram A <sriramlibra0@gmail.com>
2023-03-18 08:40:59 +0530

    interface

--- interface/interface.ts
+++ interface/interface.ts
@@ -1,15 +1,41 @@
-interface testUser{
-    name:string;
-    isPaid?:boolean;//optionsal
-    checkpaid(name:string):boolean;
-    checkCourse:(test:string)=>string;
+interface testUser {
+  name: string;
+  isPaid?: boolean; //optionsal
+  checkpaid(name: string): boolean;
+  checkCourse: (test: string) => string;
 }
 
-let testingUser:testUser;
-testingUser={
-    name:"sriram",
-    checkpaid: function(name:string){return true},
-    checkCourse:(name:string)=>{
-        return "course name"
-    }
+//reopenign of the interfaces
+
+interface testUser {
+  role: "admin" | "ta";
 }
+
+let testingUser: testUser;
+testingUser = {
+  //we have added the admin ;
+  role: "admin",
+  name: "sriram",
+  checkpaid: function (name: string) {
+    return true;
+  },
+  checkCourse: (name: string) => {
+    return "course name";
+  },
+};
+
+//inheritance
+interface newInterface extends testUser {}
+
+let newUser: newInterface;
+newUser = {
+  //we have added the admin ;
+  role: "admin",
+  name: "sriram",
+  checkpaid: function (name: string) {
+    return true;
+  },
+  checkCourse: (name: string) => {
+    return "course name";
+  },
+};
